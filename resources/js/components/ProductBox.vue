c54079e8495416163a9f77dce54ba3c3dce72621
Front-end-4 <waggner.dev@gmail.com>
2025-02-20 19:05:19 -0300

    Update ProductBox.vue

--- resources/js/components/ProductBox.vue
+++ resources/js/components/ProductBox.vue
@@ -2,8 +2,8 @@
     <div class="box-products grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
         <div class="box-product__content rounded-tl-[0rem] rounded-tr-[0rem] rounded-br-[1.25rem] rounded-bl-[1.25rem] w-100 max-w-[413.19px] relative pt-[0.5rem] pr-[1.5625rem] pb-[1.296rem] pl-[1.5625rem] border border-[#CF1E0C] border-solid" v-for="(item, index) in items" :key="index">
             <div class="box-product__image relative flex items-center justify-center w-full max-w-[362.89px] m-auto h-[215.68px] overflow-hidden">
-                <div class="tag absolute top-[1.125rem] h-[2.125rem] left-0 bg-[#CF1E0C] text-[#FFF] w-[5.063rem] flex justify-center items-center">{{ item.tag }}</div>
-                <img :src="item.image" alt="Product Image" class="w-full h-full object-cover">
+                <div class="tag z-10 absolute top-[1.125rem] h-[2.125rem] left-0 bg-[#CF1E0C] text-[#FFF] w-[5.063rem] flex justify-center items-center">{{ item.tag }}</div>
+                <img :src="item.image" alt="Product Image" class="w-full h-full object-cover transition-transform duration-700 ease-in-out transform hover:scale-110">
             </div>
             <div class="box-product__description flex flex-wrap text-left items-center gap-[1.514rem] max-w-[362.89px] m-auto">
                 <div class="box-product__description--content w-full flex flex-wrap justify-between mt-[1.363rem]">
