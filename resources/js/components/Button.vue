9022c91a7f6bb00f43d2e3631d7ef16fb3ff6acb
Front-end-4 <waggner.dev@gmail.com>
2025-02-22 15:53:08 -0300

    Update
    finish pagina produtos

--- resources/js/components/Button.vue
+++ resources/js/components/Button.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="btn">
-        <button class="btn__buy text-[#FFF] bg-[#987F2D] h-[2.188rem] w-[7.043rem] flex justify-center items-center gap-3 hover:bg-[#b8982c] noto-sans-devanagari-regular">
-            <img v-if="icon" :src="icon" alt="Product Image" class="w-[0.984rem] h-[0.833rem]">
+        <button class="btn__buy text-[#FFF] bg-[#987F2D] h-[35px] w-[7.043rem] flex justify-center items-center gap-3 hover:bg-[#b8982c] noto-sans-devanagari-regular">
+            <img v-if="icon" :src="icon" alt="Product Image" :class="imgClass">
             {{ label }}
         </button>
     </div>
@@ -10,8 +10,16 @@
 <script>
 export default {
     props: {
-        icon: String, // Apenas como prop
-        label: String
+        icon: String,
+        label: String,
+        imgClass: String,
     }
 }
 </script>
+
+<style>
+    img{
+        width: 0.984rem;
+        height: 0.833rem;
+    }
+</style>
--- resources/js/components/ProdutctFilter.vue
+++ resources/js/components/ProdutctFilter.vue
@@ -1,6 +1,11 @@
 <template>
-    <form action="" method="POST">
-        <input type="text" name="search" placeholder="Pesquise aqui">
-        <button-component :icon="'build/client/images/lupa.png'" :label="'Buscar'"></button-component>
-    </form>
+    <div class="flex flex-row items-center pt-[43px] pb-[43px]">
+        <div class="back">
+            <a href="http://" class="text-[#FFF] bg-[#987F2D] h-[35px] w-[7.043rem] flex justify-center items-center hover:bg-[#b8982c] noto-sans-devanagari-regular">Voltar</a>
+        </div>
+        <form action="" method="POST" class="flex flex-row items-end gap-0 w-full max-w-full sm:max-w-[414px] md:max-w-[430px]">
+            <input type="text" name="search" placeholder="Pesquise aqui" class="border border-white h-[35px] w-full noto-sans-devanagari-thin text-[#000] text-[0.75rem] ps-[1.125rem] focus-visible:border-[#CF1E0C] focus-visible:outline-none border border-gray-300">
+            <button-component :img-class="'object-contain aspect-[1/1]'" :icon="'build/client/images/lupa.png'" :label="'Buscar'"></button-component>
+        </form>
+    </div>
 </template>
