f346d13f83e8b4a71d8d0c84d0340386c52e5d77
Front-end-4 <waggner.dev@gmail.com>
2025-06-09 18:38:47 -0300

    Update
    Melhorias SEO

--- resources/views/client/blades/app.blade.php
+++ resources/views/client/blades/app.blade.php
@@ -13,13 +13,13 @@
     </section>       
     
     <section id="products" class="relative flex justify-center items-center">
-        <img src="{{asset('build/client/images/firula-products.png')}}" alt="Firula" class="!w-[20px] sm:w-[45px] absolute right-0 top-0">
+        <img loading="lazy" src="{{asset('build/client/images/firula-products.png')}}" alt="Firula" class="!w-[20px] sm:w-[45px] absolute right-0 top-0">
         <div class="m-auto w-[90%] max-w[79.188rem] mt-[1.875rem] sm:mt-[65.67px] mb-[1.875rem] sm:mb-[65.67px]">
             <product-box-component></product-box-component>
         </div>
     </section>
 
-    <section id="newslleter" class="newlleter w-full relative z-10">
+    <section id="newslleter" class="newlleter w-full relative z-10 h-[230px] sm:h-[376px]">
         <newslleter-component></newslleter-component>
     </section>
 @endsection
